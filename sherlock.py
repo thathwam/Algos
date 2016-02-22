984cd909db5aca2a43877aa92f4b00b79244c077
thathwam <aravinda.bhatta@gmail.com>
2016-02-21 22:57:23 -0800

    Adding a puzzle solved in HackerRank

---
+++ sherlock.py
@@ -0,0 +1,45 @@
+"""
+Sherlock Holmes suspects his archenemy, Professor Moriarty, is once again plotting something diabolical. Sherlock's companion, Dr. Watson, suggests Moriarty may be responsible for MI6's recent issues with their supercomputer, The Beast.
+
+Shortly after resolving to investigate, Sherlock receives a note from Moriarty boasting about infecting The Beast with a virus; however, he also gives him a clue—a number, NN. Sherlock determines the key to removing the virus is to find the largest Decent Number having NN digits.
+
+A Decent Number has the following properties:
+
+Its digits can only be 3's and/or 5's.
+The number of 3's it contains is divisible by 5.
+The number of 5's it contains is divisible by 3.
+If there are more than one such number, we pick the largest one.
+Moriarty's virus shows a clock counting down to The Beast's destruction, and time is running out fast. Your task is to help Sherlock find the key before The Beast is destroyed!
+
+Constraints
+1≤T≤201≤T≤20
+1≤N≤1000001≤N≤100000
+
+
+Input Format
+
+The first line is an integer, TT, denoting the number of test cases.
+
+The TT subsequent lines each contain an integer, NN, detailing the number of digits in the number.
+
+Output Format
+
+Print the largest Decent Number having NN digits; if no such number exists, tell Sherlock by printing -1
+"""
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+            Scanner in = new Scanner(System.in);
+	            int t = in.nextInt();
+		            for(int a0 = 0; a0 < t; a0++){
+			                int n = in.nextInt();
+					        }
+						    }
+						    }
+
