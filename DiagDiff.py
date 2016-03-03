6169d697fc0456581c532f9a03e0b70fd9aa1a62
thathwam <thathwam@users.noreply.github.com>
2016-03-02 16:45:04 -0800

    Submitting the answer to the puzzle diagonal diff

---
+++ DiagDiff.py
@@ -0,0 +1,26 @@
+"""
+https://www.hackerrank.com/challenges/diagonal-difference
+"""
+
+#!/bin/python3
+
+import sys
+
+n = int(input().strip())
+a = []
+for a_i in range(n):
+   a_t = [int(a_temp) for a_temp in input().strip().split(' ')]
+   a.append(a_t)
+
+leftSum = 0
+rightSum = 0
+
+for i, list in enumerate(a):
+    leftSum += list[i]
+#print(leftSum)
+
+for i, list in enumerate(a):
+    rightSum += list[n - i - 1]
+#print(rightSum)
+
+print(abs(leftSum - rightSum))
