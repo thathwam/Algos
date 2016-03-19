997d16c6262671f7ae9c5553493a08487e720fff
thathwam <thathwam@users.noreply.github.com>
2016-03-18 23:42:16 -0700

    Adding comments

--- HacherRank/DiagDiff.py
+++ HacherRank/DiagDiff.py
@@ -1,6 +1,15 @@
 """
 Find the diagonal diff in a matrix
 https://www.hackerrank.com/challenges/diagonal-difference
+Given a square matrix of size N×NN×N, calculate the absolute difference between the sums of its diagonals.
+
+Input Format
+
+The first line contains a single integer, NN. The next NN lines denote the matrix's rows, with each line containing NN space-separated integers describing the columns.
+
+Output Format
+
+Print the absolute difference between the two sums of the matrix's diagonals as a single integer.
 """
 
 #!/bin/python3
