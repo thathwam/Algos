c8643035385c84bae3f3512da17a7e3b5e87f221
thathwam <thathwam@users.noreply.github.com>
2016-03-25 23:45:13 -0700

    Create SimpleArraySum.py

---
+++ HacherRank/SimpleArraySum.py
@@ -0,0 +1,12 @@
+"""
+https://www.hackerrank.com/challenges/simple-array-sum
+"""
+#!/bin/python3
+
+import sys
+
+
+n = int(input().strip())
+arr = [int(arr_temp) for arr_temp in input().strip().split(' ')]
+
+print(sum(arr))
