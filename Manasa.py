1c1c2f3253ae3c648994a2f581a8da96b4e1d355
thathwam <thathwam@users.noreply.github.com>
2016-04-22 23:19:24 -0700

    Delete Manasa.py

--- Manasa.py
+++
@@ -1,9 +0,0 @@
-Manasa is out on a hike with friends. She finds a trail of stones with numbers on them. She starts following the trail and notices that two consecutive stones have a difference of either aa or bb. Legend has it that there is a treasure trove at the end of the trail and if Manasa can guess the value of the last stone, the treasure would be hers. Given that the number on the first stone was 00, find all the possible values for the number on the last stone.
-
-Note: The numbers on the stones are in increasing order.
-
-Input Format 
-The first line contains an integer TT, i.e. the number of test cases. TT test cases follow; each has 3 lines. The first line contains nn (the number of stones). The second line contains aa, and the third line contains bb.
-
-Output Format 
-Space-separated list of numbers which are the possible values of the last stone in increasing order.
