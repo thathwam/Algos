be7edd9084d3c01efe03385f370bbc18b7394c20
thathwam <thathwam@users.noreply.github.com>
2016-05-02 23:13:59 -0700

    Update staircase.py

--- Stack/staircase.py
+++ Stack/staircase.py
@@ -1,4 +1,11 @@
 """
+our teacher has given you the task of drawing a staircase structure. Being an expert programmer, you decided to make a program to draw it for you instead. Given the required height, can you print a staircase as shown in the example?
+
+Input 
+You are given an integer NN depicting the height of the staircase.
+
+Output 
+Print a staircase of height NN that consists of # symbols and spaces. For example for N=6N=6, here's a staircase of that height:
 https://www.hackerrank.com/challenges/staircase
 """
 #!/bin/python3
