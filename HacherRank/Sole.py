b501067f8ba238d3c9750add338c3401c0983f0f
thathwam <thathwam@users.noreply.github.com>
2016-05-22 23:52:54 -0700

    Update Sole.py

--- HacherRank/Sole.py
+++ HacherRank/Sole.py
@@ -4,16 +4,7 @@ Welcome to HackerRank! The purpose of this challenge is to familiarize you with 
 
 Review the code provided in the editor below, then complete the solveMeFirst function so that it returns the sum of two integers read from stdin. Take some time to understand this code so you're prepared to write it yourself in future challenges.
 
-Select a language below, and start coding!
-
-Input Format
-
-Code that reads input from stdin is provided for you in the editor. There are 22 lines of input, and each line contains a single integer.
-
-Output Format
-
-Code that prints the sum calculated and returned by solveMeFirst is provided for you in the editor.
-
+Select a language below, and start coding.
 """
 def solveMeFirst(a,b):
    return a + b 
