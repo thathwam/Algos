ffd32a89fec516c14ed7f788cea527c700cf5ae3
thathwam <thathwam@users.noreply.github.com>
2016-05-27 23:41:12 -0700

    Update ReturnTuple.py

--- HacherRank/ReturnTuple.py
+++ HacherRank/ReturnTuple.py
@@ -35,6 +35,6 @@ def read_visits(data_path):
         for line in f:
             yield int(line)
 
-it = read_visits('/tmp/my_numbers.txt')
+it = read_visits('/tmp/mys.txt')
 percentages = normalize(it)
 print(percentages)
